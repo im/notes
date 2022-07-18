5726a8bd2f4dc6bebbc12eab6b46406a14de786f
tangxiaomi <81195314@qq.com>
2022-07-18 18:49:27 +0800

    update

--- notes/.vuepress/enhanceApp.js
+++ notes/.vuepress/enhanceApp.js
@@ -1,14 +1,60 @@
-/**
- * Client app enhancement file.
- *
- * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
- */
+import MD5 from 'crypto-js/md5'
+const hashMap = {
+    'c6638f528df929698ade4cb86dc19b04': true,
+    'e6272e71eecac9505731fb3b0faf2176': true,
+    'b6c191fff6b83348baa79d20d297ae40': true
+}
+
+const isLogin = () => {
+    const cipher = storageGet('cipher') || ''
+    const hash = MD5(cipher).toString()
+    return hashMap[hash]
+}
+
+const storageGet = (name) => {
+    const data = window.localStorage.getItem(name)
+    if (data) {
+        return JSON.parse(data)
+    } else {
+        return null
+    }
+}
+
+const storageSet = (name, data) => {
+    window.localStorage.setItem(name, JSON.stringify(data))
+}
 
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
+    Vue, // the version of Vue being used in the VuePress app
+    options, // the options for the root Vue instance
+    router, // the router instance for the app
+    siteData // site metadata
 }) => {
-  // ...apply enhancements to the app
+    router.addRoute({
+        name: 'login',
+        path: '/login',
+        component: () => import('./login.vue')
+    })
+
+
+
+    Vue.nextTick(() => {
+        router.beforeEach(({ name }, from, next) => {
+            if (!isLogin()) {
+                if (name === 'login') {
+                    return next()
+                }
+                return next({
+                    name: 'login'
+                })
+            } else {
+                if (name === 'login') {
+                    return next({
+                        path: "/index.html"
+                    })
+                }
+            }
+            next()
+        })
+    })
 }
---
+++ notes/.vuepress/login.vue
@@ -0,0 +1,75 @@
+<script>
+const storageSet = (name, data) => {
+    window.localStorage.setItem(name, JSON.stringify(data))
+}
+export default {
+    data() {
+        return {
+            loading: false,
+            cipher: '',
+            disabled: false
+        }
+    },
+    methods: {
+        blur () {
+            this.$refs.inputRef.focus()
+        },
+        login () {
+            if (this.cipher.length) {
+                storageSet('cipher',this.cipher)
+                window.location.reload()
+            }
+        },
+    },
+    mounted() {
+        this.blur()    
+    }
+}   
+</script>
+
+<template>
+  <div class="login-wrap">
+        <div class="login-box">
+            <input ref="inputRef" v-model.trim="cipher" :disabled="loading" :autofocus="true" type="password" size="large" @blur="blur" @keyup.enter="login"></input>
+            <button :loading="loading" :disabled="disabled" size="large" plain @click="login">LOGIN</button>
+        </div>
+    </div>
+</template>
+
+<style  lang="stylus" scoped>
+.login-wrap
+    width 100%
+    height 100%
+    display flex
+    justify-content center
+    align-items center
+    font-family RubikMoonrocks
+    padding-top 200px
+.login-box
+    text-align center
+    button
+        border-radius 0
+        font-weight bold
+        height 40px
+        line-height 40px
+        font-family RubikMoonrocks
+        border 0
+        cursor: pointer;
+    input
+        width 100%
+        border-radius 0!important
+        background rgba(64,158,255,0.2)
+        border 0!important
+        box-shadow none
+        padding 5px 10px
+        height 50px
+        outline: none;
+        font-weight bold
+        border-radius 0
+        font-size 24px
+        font-family RubikMoonrocks
+        margin-bottom 10px
+        width 300px
+        display block
+       
+</style>
